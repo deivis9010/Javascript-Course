d65a0cfa8123e9bd462d6d8f86c1b6ba8636566b
@SanPanda <deivis9010@gmail.com>
2025-04-23 02:16:42 -0300

    Se realizan modificaciones para dar cumplimiento a la acttividad de la sesion 1.  Se crearon variables y scripts de impresion de mensajes en aboutme.js mientras en index.js se agrego diversos fragmentos de codigo para cumplimentar las tareas

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -1,4 +1,8 @@
-console.log("hello world");
+console.log("Respondiendo al ejercicio 1");
+let FullName = "Deivis Leal";
+let profession = "Senior Software Engineer";   
+let position = "Porject Manager at Personal Projects";
+let phrase = `Hola, me llamo ${FullName} y me dedico a ${profession}. Estoy cursando este Máster porque me gustaría trabajar en ${position}.`;
+console.log(phrase);
 
-
-console.log("laater");
+console.log("Respondiendo al ejercicio 1");
--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -1,14 +1,46 @@
+
+
 // Ejercicio 1
 import "./aboutme.js";
 console.log("Ejercicio 1");
 
 // Ejercicio 2
 
+
 // Ejercicio 3
+/**
+ * Genera un array de números del 1 al 100 y registra cada número en la consola.
+ */
+Array.from({ length: 100 }, (_, i) => i + 1).forEach(num => {
+    console.log(num);
+  });
 
 // Ejercicio 4
-
+for(var i = 0; i < 101; i++) {
+  //Solución
+}
+  console.log(i);
 // Ejercicio 5
 
 // Ejercicio 6
+const formatter = {
+  prefix: "Hello",
+  append: function (_string) {
+    console.log(this.prefix + " " + _string);
+  }
+};
+formatter.append("World");
 
+/**
+ * Se establece la propiedad "toLowerString" con enumerable en falso para evitar que esta propiedad
+ * sea incluida en las iteraciones sobre las propiedades del objeto. Esto previene errores como 
+ * "Uncaught TypeError: (a.attributes[o] || '').replace is not a function", que pueden ocurrir 
+ * cuando bibliotecas externas (como Prism.js) intentan procesar propiedades no deseadas o no estándar 
+ * del objeto durante operaciones como serialización o manipulación de atributos.
+ */
+Object.defineProperty(formatter, "toLowerString", {
+  value(str) { return str.toLowerCase(); },
+  enumerable: false
+});
+
+console.log(formatter.toLowerString("DEIVISL"));
